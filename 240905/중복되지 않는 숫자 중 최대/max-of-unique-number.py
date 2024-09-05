296b8d33d3a6f32ef77d096fb1967d33d4ef6e2e
sil <97718539+zinsile@users.noreply.github.com>
2024-09-05 15:13:23 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240905/중복되지 않는 숫자 중 최대/max-of-unique-number.py
+++ 240905/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -4,17 +4,16 @@ exs = []
 maxs = -1
 for elem in arr[1:]:
     elem = int(elem)
+    elif elem in exs:
+        elem.remove(elem)
+        continue
     if elem == maxs:
         maxs = -1
         continue
     elif elem > maxs:
         maxs = elem
         continue
-    elif elem in exs:
-        elem.remove(elem)
-        continue
-    elif elem not in exs:
-        exs.append(elem)
+    exs.append(elem)
 if maxs == -1 and len(exs)>0:
     for elem in exs:
         if elem > maxs:
