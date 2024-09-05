4a13d8bcf4e640fce0c4c78e3e7d61126cd9148e
sil <97718539+zinsile@users.noreply.github.com>
2024-09-05 16:25:17 +0900

    Title: DateTime to DateTime, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240905/DateTime to DateTime/datetime-to-datetime.py
+++ 240905/DateTime to DateTime/datetime-to-datetime.py
@@ -1,4 +1,7 @@
 a,b,c = map(int, input().split())
 time1 = 10*24*60 + 11*60 + 11
 time2 = (a-1)*24*60 + b*60 + c
-print(time2 - time1)
+result = time2 - time1
+if result > 0 :
+    print(result)
+else : print(-1)
