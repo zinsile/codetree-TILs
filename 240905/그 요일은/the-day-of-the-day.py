d463c33f6ab7fc9056ae69b0e2f2cd346b33bd68
sil <97718539+zinsile@users.noreply.github.com>
2024-09-05 17:01:03 +0900

    Title: 그 요일은, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240905/그 요일은/the-day-of-the-day.py
+++ 240905/그 요일은/the-day-of-the-day.py
@@ -1,13 +1,32 @@
 m1,d1,m2,d2 = map(int, input().split())
 day = input()
-months = [31,29,31,30,31,30,31,31,30,31,30,31]
-day1, day2 = d1, d2
-for i in range(m1-1):
-    day1 += months[i]
-for i in range(m2-1):
-    day2 += months[i]
+day1 = 0
 
-# d1 = 0
-# d2 = (day2-day1)%7
-result = (day2-day1)//7 + 1
+if day == 'Mon': day = 0
+elif day == 'Tue': day = 1
+elif day == 'Wed': day = 2
+elif day == 'Thu': day = 3
+elif day == 'Fri': day = 4
+elif day == 'Sat': day = 5
+elif day == 'Sun': day = 6
+
+
+months = [31,29,31,30,31,30,31,31,30,31,30,31]
+dn1, dn2 = d1, d2
+for i in range(m1-1):
+    dn1 += months[i]
+for i in range(m2-1):
+    dn2 += months[i]
+
+day2 = (dn2-dn1)%7
+
+# 만약 day2-day1이 day보다 작으면 0
+# 만약 day가 day2보다 작으면 //7
+# 만약 day가 day2 같거나 크면 //7 +1
+if day2-day1 < day:
+    result = 0
+elif day < day2:
+    result = (dn2-dn1)//7
+elif day >= day2:
+    result = (dn2-dn1)//7 + 1
 print(result)
