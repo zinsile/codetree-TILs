a45a46e87cada8ec14e8eede6dda66502d9554ad
sil <97718539+zinsile@users.noreply.github.com>
2024-09-05 16:48:45 +0900

    Title: 그 요일은, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240905/그 요일은/the-day-of-the-day.py
+++ 240905/그 요일은/the-day-of-the-day.py
@@ -1,11 +1,13 @@
 m1,d1,m2,d2 = map(int, input().split())
+day = input()
 months = [31,29,31,30,31,30,31,31,30,31,30,31]
-
 day1, day2 = d1, d2
 for i in range(m1-1):
     day1 += months[i]
 for i in range(m2-1):
     day2 += months[i]
 
-d = (day2-day1)%7
-print(d)
+# d1 = 0
+# d2 = (day2-day1)%7
+result = (day2-day1)//7 + 1
+print(result)
