15ff288c7bb9d2de03a6b13ae7fd6e617459439b
sil <97718539+zinsile@users.noreply.github.com>
2024-09-05 16:25:28 +0900

    Title: DateTime to DateTime, Time: 100ms, Memory: 24MB, Status: Passed - CodeTree

--- 240905/DateTime to DateTime/datetime-to-datetime.py
+++ 240905/DateTime to DateTime/datetime-to-datetime.py
@@ -2,6 +2,6 @@ a,b,c = map(int, input().split())
 time1 = 10*24*60 + 11*60 + 11
 time2 = (a-1)*24*60 + b*60 + c
 result = time2 - time1
-if result > 0 :
+if result >= 0 :
     print(result)
 else : print(-1)
