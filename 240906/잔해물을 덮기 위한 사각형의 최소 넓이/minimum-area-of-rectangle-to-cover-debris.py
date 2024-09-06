18010cbdb4ec5d845cdcaf43e6df2d7818b42a35
sil <97718539+zinsile@users.noreply.github.com>
2024-09-06 18:00:25 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 255ms, Memory: 55MB, Status: Wrong Answer - CodeTree

--- 240906/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240906/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -1,11 +1,16 @@
 info = [list(map(int, input().split())) for _ in range(2)]
 graph = [[0 for _ in range(2000)] for _ in range(2000)]
 
-for x1,y1,x2,y2 in info:
+for t,(x1,y1,x2,y2) in enumerate(info):
     x,y = 1000,1000
-    for i in range(x1,x2):
-        for j in range(y1,y2):
-            graph[x+i][y+j] += 1
+    if t == 1:
+        for i in range(x1,x2):
+            for j in range(y1,y2):
+                graph[x+i][y+j] -= 1
+    else:
+        for i in range(x1,x2):
+            for j in range(y1,y2):
+                graph[x+i][y+j] += 1
 
 minx,miny = 2000,2000
 maxx,maxy = 0,0
