2f9c126bcd16b181b809a0476e4bbe4858d75b1b
sil <97718539+zinsile@users.noreply.github.com>
2024-09-08 18:58:03 +0900

    Title: 나눗셈의 나머지, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240908/나눗셈의 나머지/remainder-of-division.py
+++ 240908/나눗셈의 나머지/remainder-of-division.py
@@ -1,10 +1,10 @@
 t = [0]*10
 a,b = tuple(map(int,input().split()))
 while True:
-    if a<=1:
-        break
     a = a//b
     t[a%b] += 1
+    if a<=1:
+        break
 result = 0
 for i in t:
     result += i**2
