39e178d45944448a0d132ce558422e9e70fa18b5
sil <97718539+zinsile@users.noreply.github.com>
2024-09-08 21:48:54 +0900

    Title: 연속부분수열일까, Time: 128ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240908/연속부분수열일까/contiguous-array-or-not.py
+++ 240908/연속부분수열일까/contiguous-array-or-not.py
@@ -6,6 +6,8 @@ cnt = 0
 for i,a in enumerate(aline):
     if a in bline:
         cnt+=1
+    elif cnt == len(bline):
+        break
     elif a not in bline and cnt>0:
         print("No")
         cnt = 0
