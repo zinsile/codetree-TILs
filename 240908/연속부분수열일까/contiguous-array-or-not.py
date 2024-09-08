ad94c03e67df74438d9acb01f824e0cb0d0470d9
sil <97718539+zinsile@users.noreply.github.com>
2024-09-08 21:57:25 +0900

    Title: 연속부분수열일까, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240908/연속부분수열일까/contiguous-array-or-not.py
+++ 240908/연속부분수열일까/contiguous-array-or-not.py
@@ -9,7 +9,13 @@ for i,a in enumerate(aline):
     else:
         if 0<cnt<n2:
             cnt = 0
+        elif cnt == n2:
+            continue
 
 if cnt == len(bline):
     print("Yes")
 else:print("No")
+
+62 10
+42 84 87 3 88 12 50 62 45 100 8 98 85 20 1 21 18 66 67 59 47 17 34 77 38 66 87 95 59 59 43 23 64 86 77 37 14 2 96 85 3 27 40 49 16 28 42 98 43 39 63 39 62 33 17 57 47 2 59 54 28 23 
+23 64 86 77 37 14 2 96 85 3
