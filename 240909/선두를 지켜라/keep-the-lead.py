1c16153199430e028515a76c81b62f3afb2a2637
sil <97718539+zinsile@users.noreply.github.com>
2024-09-09 11:48:09 +0900

    Title: 선두를 지켜라, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240909/선두를 지켜라/keep-the-lead.py
+++ 240909/선두를 지켜라/keep-the-lead.py
@@ -3,26 +3,26 @@ a_moving = [list(map(int, input().split())) for _ in range(n)]
 b_moving = [list(map(int, input().split())) for _ in range(m)]
 a_pos,b_pos = [0]*10**6,[0]*10**6
 
-p,h = 0,0
+ap,h = 0,0
 for v,t in a_moving:
     for i in range(t):
         for j in range(v):
-            a_pos[p+j+1] = h+i+1
-        p = p+v
+            a_pos[ap+j+1] = h+i+1
+        ap = ap+v
     h = h+t
 
-p,h = 0,0
+bp,h = 0,0
 for v,t in b_moving:
     for i in range(t):
         for j in range(v):
-            b_pos[p+j+1] = h+i+1
-        p = p+v
+            b_pos[bp+j+1] = h+i+1
+        bp = bp+v
     h = h+t
 
 first_runner = 'N'
 cnt = 0
 for i in range(1,10**6):
-    if i > h:
+    if i > min(ap,bp):
         break
     elif a_pos[i] == b_pos[i]:
         continue
@@ -32,4 +32,5 @@ for i in range(1,10**6):
     elif a_pos[i]>b_pos[i] and (first_runner=='a'or first_runner=='N'):
         cnt += 1
         first_runner = 'b'
+    # print(first_runner)
 print(cnt-1)
