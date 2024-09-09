0feeb9f2c21f9515ea8741ad467e3b841195329c
sil <97718539+zinsile@users.noreply.github.com>
2024-09-09 11:06:57 +0900

    Title: 벌금은 누구에게, Time: 148ms, Memory: 25MB, Status: Passed - CodeTree

--- 240909/벌금은 누구에게/who-will-pay.py
+++ 240909/벌금은 누구에게/who-will-pay.py
@@ -1,12 +1,19 @@
-n,m,k = map(int, input().split())
-y_card = [0]*(n+1)
-temp = False
-for i in range(m):
-    num = int(input())
-    y_card[num] += 1
-    if k in y_card:
-        print(y_card.index(k))
-        temp = True
+# 변수 선언 및 입력
+n, m, k = tuple(map(int, input().split()))
+penalized_person = [
+    int(input())
+    for _ in range(m)
+]
+penalty_num = [0] * (n + 1)
+
+# 각 패널티 횟수를 세서,
+# 최초로 K번 이상 벌칙을 받는 사람을 추적합니다.
+ans = -1
+for target in penalized_person:
+    penalty_num[target] += 1
+
+    if penalty_num[target] >= k:
+        ans = target
         break
-if not temp:
-    print(-1)
+
+print(ans)
