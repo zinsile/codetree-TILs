a5d77828945fe03997eb28800bac9ae6e66308d6
sil <97718539+zinsile@users.noreply.github.com>
2024-09-09 17:58:41 +0900

    Title: 격자 반대로 채우기, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

--- 240909/격자 반대로 채우기/grid-anti-fill.py
+++ 240909/격자 반대로 채우기/grid-anti-fill.py
@@ -2,8 +2,9 @@ n = int(input())
 graph = [[0 for _ in range(n)] for _ in range(n)]
 
 num = 1
+cnt = 0
 for i in range(n-1,-1,-1):
-    if i%2==1:
+    if cnt%2==0:
         for j in range(n-1,-1,-1):
             graph[j][i] = num
             num+=1
@@ -11,6 +12,7 @@ for i in range(n-1,-1,-1):
         for j in range(n):
             graph[j][i] = num
             num+=1
+    cnt +=1
 
 for raw in graph:
     for elem in raw:
