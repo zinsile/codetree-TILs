788414ce6572243c2bde833aa5040e981fbd989d
sil <97718539+zinsile@users.noreply.github.com>
2024-09-09 13:29:11 +0900

    Title: 좌우로 움직이는 로봇, Time: 215ms, Memory: 41MB, Status: Passed - CodeTree

--- 240909/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 240909/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -21,10 +21,12 @@ for _ in range(m):
         if d=='L':
             pos_b[present_b] = pos_b[present_b-1] -1
             present_b += 1
+
         else:
             pos_b[present_b] = pos_b[present_b-1] +1
             present_b += 1
 
+present_a,present_b = present_a-1, present_b-1
 #먼저 끝난 로봇은 다른 로봇이 끝나기 전까지 같은 위치로 설정
 for i in range(abs(present_a-present_b)):
     if present_a>present_b:
