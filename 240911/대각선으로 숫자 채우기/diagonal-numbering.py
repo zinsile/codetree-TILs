418a231f2987e5299cf93534ac158405c504b93a
sil <97718539+zinsile@users.noreply.github.com>
2024-09-11 16:57:03 +0900

    Title: 대각선으로 숫자 채우기, Time: 151ms, Memory: 25MB, Status: Passed - CodeTree

--- 240911/대각선으로 숫자 채우기/diagonal-numbering.py
+++ 240911/대각선으로 숫자 채우기/diagonal-numbering.py
@@ -1,29 +1,40 @@
-n,m = map(int, input().split())
-graph = [[0 for _ in range(m)] for _ in range(n)]
+# 변수 선언 및 입력
+n, m = tuple(map(int, input().split()))
+answer = [
+    [0 for _ in range(m)]
+    for _ in range(n)
+]
+count = 1
 
-num = 1
-for j in range(m):
-    t = 0
-    for i in range(0,j+1):
-        if j==-1:
-            continue
-        graph[t][j] = num
-        num+=1
-        j -= 1
-        t += 1
+        
+# Step 1:
+for start_col in range(m):
+    curr_row = 0
+    curr_col = start_col
 
-for j in range(1,n):
-    t = j
-    k = n-1
-    for i in range(0,j+1):
-        if t==n:
-            continue
-        graph[t][k] = num
-        num+=1
-        t+=1
-        k-=1
+    while 0 <= curr_col and curr_row < n:
+        answer[curr_row][curr_col] = count
+        
+        # 변수 업데이트
+        curr_row += 1
+        curr_col -= 1
+        count += 1
 
-for raw in graph:
-    for elem in raw:
-        print(elem, end=' ')
+# Step 2:
+for start_row in range(1, n):
+    curr_row = start_row
+    curr_col = m - 1
+
+    while 0 <= curr_col and curr_row < n:
+        answer[curr_row][curr_col] = count
+        
+        # 변수 업데이트
+        curr_row += 1
+        curr_col -= 1
+        count += 1
+
+# 출력:
+for row in range(n):
+    for col in range(m):
+        print(answer[row][col], end = ' ')
     print()
