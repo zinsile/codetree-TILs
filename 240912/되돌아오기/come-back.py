c44f4ea24b2073530f81eca08b3601d69eae358e
sil <97718539+zinsile@users.noreply.github.com>
2024-09-12 14:36:51 +0900

    Title: 되돌아오기, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240912/되돌아오기/come-back.py
@@ -0,0 +1,19 @@
+n = int(input())
+mapper = {'N':0,'E':1,'S':2,'W':3}
+dxs, dys = [-1,0,1,0],[0,1,0,-1]
+x,y = 0,0
+time = 1
+end = False
+
+for _ in range(n):
+    d,t = input().split()
+    dir_num = mapper[d]
+    for _ in range(int(t)):
+        x,y = x+dxs[dir_num], y+dys[dir_num]
+        if x==0 and y==0:
+            print(time)
+            end = True
+            break
+        time+=1
+if not end:
+    print(-1)
