16d1f333161b02a84ee70d5dd305043e06b5f9de
sil <97718539+zinsile@users.noreply.github.com>
2024-09-12 13:43:27 +0900

    Title: 문자에 따른 명령 2, Time: 126ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240912/문자에 따른 명령 2/text-based-commands2.py
@@ -0,0 +1,14 @@
+arr = list(input())
+dx,dy = [0,1,0,-1],[1,0,-1,0]
+x,y = 0,0
+dic_num = 0
+
+for t in arr:
+    if t=="F":
+        x,y = x+dx[dic_num], y+dy[dic_num]
+    elif t=="L":
+        dic_num = (dic_num-1+4)%4
+    elif t=="R":
+        dic_num = (dic_num+1)%4
+
+print(x,y,end=' ')
