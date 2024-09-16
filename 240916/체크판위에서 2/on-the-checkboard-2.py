127c9c6d9434fd82ddae762384f15c77cfe02f9c
sil <97718539+zinsile@users.noreply.github.com>
2024-09-16 14:50:08 +0900

    Title: 체크판위에서 2, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240916/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,12 @@
+r,c = map(int, input().split())
+graph = [list(input().split()) for _ in range(r)]
+
+cnt = 0
+for i in range(1,r-1):
+    for j in range(1,c-1):
+        for k in range(i+1,r-1):
+            for l in range(j+1,c-1):
+                if graph[i][j]!=graph[0][0] and graph[k][l]!=graph[r-1][c-1]:
+                    cnt+=1
+
+print(cnt)
