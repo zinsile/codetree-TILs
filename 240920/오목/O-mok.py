1ea7522ae63670fba9ad4085dabdb867beb232ac
sil <97718539+zinsile@users.noreply.github.com>
2024-09-20 15:13:40 +0900

    Title: 오목, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240920/오목/O-mok.py
+++ 240920/오목/O-mok.py
@@ -1,52 +1,36 @@
-graph = [list(map(int, input().split())) for _ in range(19)]
+import sys
 
-winner,x,y = 0,0,0
-t = False
+arr = [list(map(int, input().split())) for  _ in range(19)]
+
+def in_range(x,y):
+    return x>=0 and x<19 and y>=0 and y<19
+
+dxs,dys = [-1,-1,-1,0,1,1,1,0],[-1,0,1,1,1,0,-1,-1]
+
 for i in range(19):
     for j in range(19):
-        if graph[i][j] == 0:
+
+        if arr[i][j]==0:
             continue
-        cnt = 1
-        #가로
-        for k in range(1,5):
-            if j+k<19 and graph[i][j] == graph[i][j+k]:
-                cnt += 1
-        if cnt == 5:
-            winner = graph[i][j]
-            x,y = i+1,j+3
-            t = True
-        cnt = 1
-        #세로
-        if not t:
-            for k in range(1,5):
-                if i+k<19 and graph[i][j] == graph[i+k][j]:
-                    cnt += 1
-            if cnt == 5:
-                winner = graph[i][j]
-                x,y = i+3,j+1
-                t = True
-        cnt = 1
-        #왼대각선
-        if not t:
-            for k in range(1,5):
-                if i+k<19 and j+k<19 and graph[i][j] == graph[i+k][j+k]:
-                    cnt += 1
-            if cnt == 5:
-                winner = graph[i][j]
-                x,y = i+3,j+3
-                t = True
-        cnt = 1
-        #오대각선
-        if not t:
-            for k in range(1,5):
-                if i+k<19 and j-k>=0 and graph[i][j] == graph[i+k][j-k]:
-                    cnt += 1
-            if cnt == 5:
-                winner = graph[i][j]
-                x,y = i+3,j-1
-                t = True
-if winner == 0:
-    print(0)
-else:
-    print(winner)
-    print(x,y,end=' ')
+        
+        for dx,dy in zip(dxs,dys):
+            curt = 1
+            curx = i
+            cury = j
+            while True:
+                nx = curx + dx
+                ny = cury + dy
+                if not in_range(nx,ny):
+                    break
+                if arr[i][j] != arr[nx][ny]:
+                    break
+                curt+=1
+                curx = nx
+                cury = ny
+            
+            if curt == 5:
+                print(arr[i][j])
+                print(i+2*dx+1, j+2*dy+1)
+                sys.exit()
+
+print(0)
