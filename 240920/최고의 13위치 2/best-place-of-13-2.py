da83152313e129d85a5a6da566aac3afbf40923c
sil <97718539+zinsile@users.noreply.github.com>
2024-09-20 16:55:17 +0900

    Title: 최고의 13위치 2, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240920/최고의 13위치 2/best-place-of-13-2.py
@@ -0,0 +1,19 @@
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+def in_range(x,y):
+    return x>=0 and x<n and y>=0 and y<n
+
+curmax = 0
+
+for i in range(n):
+    for j in range(n-2):
+        for k in range(i,n):
+            for l in range(n-2):
+                if i==k and j<=l<j+3:
+                    l = l+(j+3-l)
+                if not in_range(l+1,l+2):
+                    continue
+                curmax = max(curmax, arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[k][l]+arr[k][l+1]+arr[k][l+2])
+
+print(curmax)
