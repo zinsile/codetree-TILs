c387f119a3784d99ddec625ee3148a418d9ebc10
sil <97718539+zinsile@users.noreply.github.com>
2024-09-23 22:20:57 +0900

    Title: 개발자의 능력 3, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 240923/개발자의 능력 3/ability-of-developer-3.py
+++ 240923/개발자의 능력 3/ability-of-developer-3.py
@@ -2,15 +2,16 @@ arr = list(map(int, input().split()))
 
 minpower = 3000000
 
-def diffsum(i,j):
-    sum1 = arr[i]+arr[j]
+def diffsum(i,j,k):
+    sum1 = arr[i]+arr[j]+arr[k]
     sum2 = sum(arr) - sum1
     diff = abs(sum1-sum2)
     return diff
 
 for i in range(6):
     for j in range(i+1,6):
-        diff = diffsum(i,j)
-        minpower = min(minpower, diff)
+        for k in range(j+1,6):
+            diff = diffsum(i,j,k)
+            minpower = min(minpower, diff)
 
 print(minpower)
