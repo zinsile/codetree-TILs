8cb8d46656593bee880a195392d836085641dd03
sil <97718539+zinsile@users.noreply.github.com>
2024-09-25 16:13:51 +0900

    Title: 바구니 안의 사탕 2, Time: 151ms, Memory: 28MB, Status: Passed - CodeTree

--- 240925/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240925/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -5,12 +5,17 @@ for _ in range(n):
     arr[p] += t
 
 def in_range(i):
-    return 0>=i and 101>=i
+    return 0<=i and 100>=i
 
 maxcan = 0
-for i in range(k,100-k+1):
-    subcan = sum(arr[i-k:i+k+1])
-    # print(subcan)
+for i in range(101):
+    subcan = arr[i]
+    for j in range(k):
+        j += 1
+        if in_range(i-j):
+            subcan += arr[i-j]
+        if in_range(i+j):
+            subcan += arr[i+j]
     maxcan = max(maxcan, subcan)
 
 print(maxcan)
