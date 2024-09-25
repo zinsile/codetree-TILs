404f8fb43247ff251b1eb0ecbd290f50e2c86a68
sil <97718539+zinsile@users.noreply.github.com>
2024-09-25 16:14:43 +0900

    Title: 바구니 안의 사탕 2, Time: 113ms, Memory: 73MB, Status: Passed - CodeTree

--- 240925/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240925/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,21 +1,23 @@
-n,k = map(int, input().split())
-arr = [0]*101
+MAX_NUM = 100
+
+# 변수 선언 및 입력
+n, k = tuple(map(int, input().split()))
+arr = [0] * (MAX_NUM + 1)
+
 for _ in range(n):
-    t,p = map(int, input().split())
-    arr[p] += t
-
-def in_range(i):
-    return 0<=i and 100>=i
-
-maxcan = 0
-for i in range(101):
-    subcan = arr[i]
-    for j in range(k):
-        j += 1
-        if in_range(i-j):
-            subcan += arr[i-j]
-        if in_range(i+j):
-            subcan += arr[i+j]
-    maxcan = max(maxcan, subcan)
-
-print(maxcan)
+    a, x = tuple(map(int, input().split()))
+    arr[x] += a
+	
+# 모든 구간의 시작점을 잡아봅니다.
+max_sum = 0
+for i in range(MAX_NUM):
+	# 해당 구간 내 원소의 합을 구합니다.
+	sum_all = 0
+	for j in range(i - k, i + k + 1):
+		if j >= 0 and j <= MAX_NUM:
+			sum_all += arr[j]
+        
+    # 최댓값을 구합니다.
+	max_sum = max(max_sum, sum_all)
+                        
+print(max_sum)
