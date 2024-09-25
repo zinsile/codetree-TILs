7aeb2f977afe1e5df312d43c4904b003c2e13d18
sil <97718539+zinsile@users.noreply.github.com>
2024-09-25 16:46:52 +0900

    Title: G or H 2, Time: 134ms, Memory: 25MB, Status: Passed - CodeTree

--- 240925/G or H 2/G-or-H-2.py
+++ 240925/G or H 2/G-or-H-2.py
@@ -1,43 +1,29 @@
-arr = [0]*101
+MAX_NUM = 100
+
 n = int(input())
-R = 0
+arr = [0] * (MAX_NUM + 1)
+
 for _ in range(n):
-    p,alpha = input().split()
-    R = max(R,int(p))
-    if alpha=='G':
-        arr[int(p)] = 1
-    else: arr[int(p)] = 2
+    x, c = input().split()
+    x = int(x)
+    arr[x] = 1 if c == 'G' else 2
 
-maxg, maxh, maxgh = 0,0,0
-for i in range(R+1):
-    for j in range(i,R+1):
-        temp = False
-        for k in range(i,j+1):
-            if arr[k]==2:
-                temp = True
-        if not temp:
-            maxg = max(maxg,j-i)
+max_len = 0
+for i in range(MAX_NUM + 1):
+    for j in range(i+1, MAX_NUM + 1):
 
-for i in range(R+1):
-    for j in range(i,R+1):
-        temp = False
-        for k in range(i,j+1):
-            if arr[k]==1:
-                temp = True
-        if not temp:
-            maxh = max(maxh,j-i)
+        if arr[i] == 0 or arr[j] == 0:
+            continue
+        
+        cnt_g, cnt_h = 0, 0
 
-for i in range(R+1):
-    for j in range(i,R+1):
-        g,h = 0,0
-        for k in range(i,j+1):
-            if arr[k]==1:
-                g+=1
-            else: h+=1
-        if g==h:
-            maxgh = max(maxgh,j-i)
+        for k in range(i, j+1):
+            if arr[k] == 1:
+                cnt_g += 1
+            if arr[k] == 2:
+                cnt_h += 1
+            
+        if cnt_g == 0 or cnt_h == 0 or cnt_g == cnt_h:
+            max_len = max(max_len, j-i)
 
-if n==1:
-    print(0)
-else:
-    print(max(maxg,maxh,maxgh))
+print(max_len)
