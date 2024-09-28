7f250af7618077676199897ed010c569c7404934
sil <97718539+zinsile@users.noreply.github.com>
2024-09-28 17:45:19 +0900

    Title: 스승의 은혜 3, Time: 227ms, Memory: 26MB, Status: Passed - CodeTree

--- 240928/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240928/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -3,16 +3,15 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 
 max_student = 0
 for i in range(n):
-    tmp = [arr[j] for j in range(n)]
-    tmp[i][0] = tmp[i][0]/2
-    tmp = [a+b for a,b in tmp]
+    arr[i][0] = arr[i][0]/2
+    tmp = [a+b for a,b in arr]
     price = 0
     student = 0
     for p in sorted(tmp):
-        if price+p>b:
-            break
-        price += p
-        student += 1
+        if price+p<=b:
+            price += p
+            student += 1
     max_student = max(max_student, student)
+    arr[i][0] = arr[i][0]*2
 
 print(max_student)
