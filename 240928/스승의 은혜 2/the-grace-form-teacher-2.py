7523b45fc8229989235aeaa2400e20db8d31a97c
sil <97718539+zinsile@users.noreply.github.com>
2024-09-28 17:03:06 +0900

    Title: 스승의 은혜 2, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240928/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -0,0 +1,15 @@
+n, b = map(int, input().split())
+arr = [int(input()) for _ in range(n)]
+
+max_people = 0
+for i in range(n):
+    arr[i] = arr[i]//2
+    price, people = 0, 0
+    for j in sorted(arr):
+        price += j
+        if price<b:
+            people += 1
+    max_people = max(max_people, people)
+    arr[i] = arr[i]*2
+
+print(max_people)
