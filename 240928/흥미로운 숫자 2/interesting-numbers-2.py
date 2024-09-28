87a975578d6fe6fdc680b964fc5f661babab0e59
sil <97718539+zinsile@users.noreply.github.com>
2024-09-28 18:43:32 +0900

    Title: 흥미로운 숫자 2, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240928/흥미로운 숫자 2/interesting-numbers-2.py
+++ 240928/흥미로운 숫자 2/interesting-numbers-2.py
@@ -3,11 +3,19 @@ x,y = map(int, input().split())
 cnt = 0
 for i in range(x,y+1):
     arr = list(map(int, list(str(i))))
-    t = []
+    tmp = [0]*10
+    tmp2 = []
     for a in arr:
-        if a not in t:
-            t.append(a)
-    if len(t) == 2:
-        cnt += 1
+        if a not in tmp2:
+            tmp2.append(a)
+        tmp[a] += 1
+    r = False
+    if len(tmp2) == 2:
+        for t in tmp2:
+            if tmp[t] == 1 or tmp[t] == len(arr)-1:
+                r = True
+    if r:
+        print(i)
+        cnt+=1
 
 print(cnt)
