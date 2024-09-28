6ceb01df965c5f850823f73db7a53942f497c076
sil <97718539+zinsile@users.noreply.github.com>
2024-09-29 02:30:40 +0900

    Title: 팰린드롬 수 찾기, Time: 242ms, Memory: 25MB, Status: Passed - CodeTree

--- 240929/팰린드롬 수 찾기/find-the-number-of-palindrome.py
+++ 240929/팰린드롬 수 찾기/find-the-number-of-palindrome.py
@@ -1,11 +1,16 @@
-x,y = map(int, input().split())
+# 변수 선언 및 입력:
+x, y = tuple(map(int, input().split()))
+ans = 0
 
-cnt = 0
-for i in range(x,y+1):
-    arr = list(str(i))
-    tmp1 = [int(a) for a in arr]
-    tmp2 = [int(a) for a in arr[::-1]]
-    if tmp1==tmp2:
-        cnt += 1
+# 각 정수에 대해 
+# 팰린드롬 수인지 아닌지 여부를 판단합니다.
+for i in range(x, y + 1):
+    # 정수 형태를 문자열 배열로 바꿉니다.
+    str_i = str(i)
 
-print(cnt)
+    # 펠린드롬 수가 되기 위해서는,
+    # 거꾸로 읽어도 똑같은 수여야 합니다.
+    if str_i == str_i[::-1]:
+        ans += 1
+
+print(ans)
