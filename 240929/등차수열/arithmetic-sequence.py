e9d26a5a8ba1d5fe47f6e754f6a16ff3a9eecf02
sil <97718539+zinsile@users.noreply.github.com>
2024-09-29 02:02:12 +0900

    Title: 등차수열, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

--- 240929/등차수열/arithmetic-sequence.py
+++ 240929/등차수열/arithmetic-sequence.py
@@ -1,15 +1,23 @@
+MAX_A = 100
+
+# 변수 선언 및 입력
 n = int(input())
+
 arr = list(map(int, input().split()))
 
-cnt = [0]*101
-for i in range(n):
-    for j in range(i+1,n):
-        if arr[i]>arr[j]:
-            for k in range(arr[i]-1,arr[j],-1):
-                if (arr[i]-k) == (k-arr[j]):
-                    cnt[k] += 1
-        else:
-            for k in range(arr[i]+1,arr[j]):
-                if (k-arr[i]) == (arr[j]-k):
-                    cnt[k] += 1
-print(max(cnt))
+ans = 0
+
+# 각 숫자에 대해 
+# 등차수열의 개수를 확인합니다.
+for x in range(1, MAX_A + 1):
+	# 모든 쌍을 만들어 등차수열의 개수를 확인합니다.
+    cnt = 0
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            if arr[i] + arr[j] == 2 * x:
+                cnt += 1
+
+    ans = max(ans, cnt)
+    
+print(ans)
