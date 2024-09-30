2f104c3c8c8e9737f6f83947ac333c2ca3195348
sil <97718539+zinsile@users.noreply.github.com>
2024-09-30 15:10:09 +0900

    Title: 숫자 2배 후 하나 제거하기, Time: 148ms, Memory: 25MB, Status: Passed - CodeTree

--- 240930/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.py
+++ 240930/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.py
@@ -3,17 +3,18 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 min_diff = sys.maxsize
+
 for i in range(n):
     arr[i] *= 2
     for j in range(n):
         remaining = []
         for k in range(n):
-            if i!=k:
+            if j!=k:
                 remaining.append(arr[k])
         sub_diff = 0
         for l in range(1,n-1):
-            sub_diff += abs(arr[l-1]-arr[l])
+            sub_diff += abs(remaining[l-1]-remaining[l])
         min_diff = min(min_diff,sub_diff)
-
     arr[i] //= 2
+
 print(min_diff)
