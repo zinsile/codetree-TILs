17c42c70e2033d2328f8f61a0de57d8de4781c1d
sil <97718539+zinsile@users.noreply.github.com>
2024-10-01 16:49:07 +0900

    Title: 트로미노, Time: 215ms, Memory: 26MB, Status: Passed - CodeTree

--- 241001/트로미노/tromino.py
+++ 241001/트로미노/tromino.py
@@ -6,15 +6,22 @@ block = [[(0,-1),(0,1)],[(-1,0),(1,0)],[(1,0),(0,1)],[(0,-1),(1,0)],[(-1,0),(0,-
 def in_range(x,y):
     return x>=0 and x<n and y>=0 and y<m
 
+def get_max_sum(x,y):
+    max_get_score = 0
+    for b in block:
+            score = graph[x][y]
+            for cx,cy in b:
+                if not in_range(x+cx,y+cy):
+                    break
+                score += graph[x+cx][y+cy]
+            max_get_score = max(max_get_score,score)
+    return max_get_score
+            
+
 max_score = 0
 for i in range(n):
     for j in range(m):
-        for b in block:
-            score = graph[i][j]
-            for cx,cy in b:
-                if not in_range(i+cx,j+cy):
-                    break
-                score += graph[i+cx][j+cy]
-            max_score = max(max_score, score)
+        score = get_max_sum(i,j)
+        max_score = max(max_score, score)
 
 print(max_score)
