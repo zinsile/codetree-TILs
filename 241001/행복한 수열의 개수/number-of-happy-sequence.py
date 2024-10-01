f7f00d6abf28d91f5b9d0f9a4cf5973036f0e9fb
sil <97718539+zinsile@users.noreply.github.com>
2024-10-01 15:35:21 +0900

    Title: 행복한 수열의 개수, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241001/행복한 수열의 개수/number-of-happy-sequence.py
+++ 241001/행복한 수열의 개수/number-of-happy-sequence.py
@@ -5,24 +5,28 @@ result = 0
 for i in range(n):# 가로
     prev = graph[i][0]
     cnt = 0
+    max_cnt = 0
     for j in range(n):
+        max_cnt = max(max_cnt,cnt)
         if prev == graph[i][j]:
             cnt += 1
         if prev != graph[i][j]:
             prev = graph[i][j]
             cnt = 1
-    if cnt >= m:
+    if max_cnt >= m:
         result += 1
     
     prev = graph[0][i]
     cnt = 0
+    max_cnt= 0
     for j in range(n):
+        max_cnt = max(max_cnt,cnt)
         if prev == graph[j][i]:
             cnt += 1
         if prev != graph[j][i]:
             prev = graph[j][i]
             cnt = 1
-    if cnt >= m:
+    if max_cnt >= m:
         result += 1
 
 print(result)
