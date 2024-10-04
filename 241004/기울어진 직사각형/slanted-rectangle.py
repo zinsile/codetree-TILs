8d61d54f73f0ee233ce30f507d1b3f1ffaccc405
sil <97718539+zinsile@users.noreply.github.com>
2024-10-04 22:50:18 +0900

    Title: 기울어진 직사각형, Time: 259ms, Memory: 26MB, Status: Passed - CodeTree

--- 241004/기울어진 직사각형/slanted-rectangle.py
+++ 241004/기울어진 직사각형/slanted-rectangle.py
@@ -4,61 +4,28 @@ graph = [
     for _ in range(n)
 ]
 
-def calculate(r,c,w,h):
+def in_range(x,y):
+    return x>=0 and x<n and y>=0 and y<n
+
+
+def get_score(x,y,w,h):
+    move_nums = [w,h,w,h]
+    dxs,dys = [-1,-1,1,1],[1,-1,-1,1]
     score = 0
-    H,W= 0,0
-    if h==1:
-        H=1
-    if w==1:
-        W=1
-    if h>1:
-        H=h-1
-    if w>1:
-        W=w-1
-    for _ in range(W):
-        score += graph[r-1][c+1]
-        r,c = r-1,c+1
-    for _ in range(H):
-        score += graph[r-1][c-1]
-        r,c = r-1,c-1
-    for _ in range(W):
-        score += graph[r+1][c-1]
-        r,c = r+1,c-1
-    for _ in range(H):
-        score += graph[r+1][c+1]
-        r,c = r+1,c+1
+
+    for num,dx,dy in zip(move_nums,dxs,dys):
+        for n in range(num):
+            if not in_range(x+dx,y+dy):
+                return 0
+            x, y = x+dx, y+dy
+            score += graph[x][y]
     return score
 
-def square(i, j):
-    max_score = 0
-    for k in range(2,n+1):
+ans = 0
+for i in range(n):
+    for j in range(n):
         for w in range(1,n):
-            h = k-w+1
-            if w>=k:
-                continue
-            H,W= 0,0
-            if h==1:
-                H=1
-            if w==1:
-                W=1
-            if h>1:
-                H=h-1
-            if w>1:
-                W=w-1
-            if i-(W)<0 or i-(W)>=n or j+(W)<0 or j+(W)>=n:
-                continue
-            if i-(W)-H<0 or i-(W)-H>=n or j+(W)-H<0 or j+(W)-H>=n:
-                continue
-            if i-H<0 or i-H>=n or j-H<0 or j-H>=n:
-                continue
-            score = calculate(i,j,w,h)
-            max_score = max(max_score, score)
-    return max_score
+            for h in range(1,n):
+                ans = max(ans,get_score(i,j,w,h))
 
-max_result = 0
-for row in range(n):
-    for col in range(n):
-        result = square(row, col)
-        max_result = max(max_result, result)
-
-print(max_result)
+print(ans)
