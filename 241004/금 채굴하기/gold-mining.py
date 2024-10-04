9c54e159e07a15c216a06b945fd197c694f662ff
sil <97718539+zinsile@users.noreply.github.com>
2024-10-04 20:59:51 +0900

    Title: 금 채굴하기, Time: 560ms, Memory: 29MB, Status: Passed - CodeTree

--- 241004/금 채굴하기/gold-mining.py
+++ 241004/금 채굴하기/gold-mining.py
@@ -1,23 +1,28 @@
 n, m = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
+graph = [
+    list(map(int, input().split())) 
+    for _ in range(n)
+]
 
-def get_gold_cnt(row, col):
-    max_cnt = 0
-    for k in range(2*(n-1)+1):
-        cnt = 0
-        for i in range(n):
-            for j in range(n):
-                if abs(row-i)+abs(col-j)>k:
-                    continue
-                cnt += graph[i][j]
-        if k*k+(k+1)*(k+1) <= cnt * m:
-            max_cnt = max(max_cnt, cnt)
-    return max_cnt
+def get_area(k):
+    return k*k + (k+1)*(k+1)
+
+def get_gold(r,c,k):
+    cnt = sum([
+        graph[i][j]
+        for i in range(n)
+        for j in range(n)
+        if abs(r-i)+abs(c-j) <= k
+    ])
+    return cnt
 
 max_gold = 0
 for row in range(n):
     for col in range(n):
-        gold_cnt = get_gold_cnt(row,col)
-        max_gold = max(max_gold, gold_cnt)
+        for k in range(2*(n-1) + 1):
+            gold_cnt = get_gold(row, col, k)
+
+            if gold_cnt * m >= get_area(k):
+                max_gold = max(max_gold, gold_cnt)
 
 print(max_gold)
