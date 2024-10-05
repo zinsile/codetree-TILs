541e7827e2ed324f83d34bdd1f91d3093639c284
sil <97718539+zinsile@users.noreply.github.com>
2024-10-05 17:25:52 +0900

    Title: 1차원 젠가, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 241005/1차원 젠가/jenga-1d.py
+++ 241005/1차원 젠가/jenga-1d.py
@@ -1,23 +1,28 @@
 n = int(input())
-arr = [0]*(n+1)
-for i in range(1,n+1):
-    arr[i] = int(input())
+arr = [
+    int(input())
+    for _ in range(n)
+]
 
-def block_delete(s,e):
-    for i in range(s,e+1):
-        arr[i] = 0
-    temp = [0]
-    for a in arr:
-        if a>0:
-            temp.append(a)
-    return temp
+end_of_range = n
+
+def cut_array(s_idx, e_idx):
+    
+    global end_of_range, arr
+
+    temp = []
+    for i in range(end_of_range):
+        if i<s_idx or i>e_idx:
+            temp.append(arr[i])
+
+    end_of_range = len(temp)
+    for i in range(end_of_range):
+        arr[i] = temp[i]
 
 for _ in range(2):
     s, e = map(int, input().split())
-    #블럭 빼기
-    arr = block_delete(s,e)
+    cut_array(s-1, e-1)
 
-print(len(arr)-1)
-for t in arr:
-    if t != 0:
-        print(t)
+print(end_of_range)
+for i in range(end_of_range):
+    print(arr[i])
