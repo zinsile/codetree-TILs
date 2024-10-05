1219fe79663f9dde032f306e091a3670f1419657
sil <97718539+zinsile@users.noreply.github.com>
2024-10-05 16:10:29 +0900

    Title: 최단 Run Length 인코딩, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 241005/최단 Run Length 인코딩/shortest-run-length-encoding.py
+++ 241005/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -1,13 +1,20 @@
 arr = list(input())
 
 def count(arr):
-    t = 0
-    cnt = 0
-    for i in arr:
+    t, cnt, result = arr[0], 1, 0
+    for i in arr[1:]:
         if i != t:
+            if cnt<10:
+                result += 2
+            else : resut += 3
+            t, cnt = i, 1
+        if i == t:
             cnt+=1
-            t = i
-    return 2*cnt
+    if cnt < 10:
+        result += 2
+    else : result += 3
+        
+    return result
 
 
 def simulate():
@@ -17,5 +24,5 @@ def simulate():
         min_length = min(min_length, count(arr))
     return min_length
 
-result = simulate()
-print(result)
+ans = simulate()
+print(ans)
