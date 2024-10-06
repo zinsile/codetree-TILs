6d69424ced27d0a7e01068af6d052569675b5331
sil <97718539+zinsile@users.noreply.github.com>
2024-10-06 19:54:49 +0900

    Title: 십자 모양의 지속적 폭발, Time: 212ms, Memory: 26MB, Status: Passed - CodeTree

--- 241006/십자 모양의 지속적 폭발/cross-shape-continuous-bomb.py
+++ 241006/십자 모양의 지속적 폭발/cross-shape-continuous-bomb.py
@@ -1,48 +1,65 @@
-n,m = map(int, input().split())
-grid = [list(map(int, input().split())) for _ in range(n)]
+OUT_OF_GRID = -1
 
-def shift_down():
-    temp_grid = [[0 for _ in range(n)] for _ in range(n)]
+# 변수 선언 및 입력:
+n, m = tuple(map(int, input().split()))
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+next_grid = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
 
+
+def get_bomb_row(bomb_col):
+    for i in range(n):
+        if grid[i][bomb_col] != 0:
+            return i
+    return OUT_OF_GRID
+
+def bomb_in_range(x,y,center_x,center_y, number):
+    return (x==center_x or y==center_y) and \
+    abs(x-center_x)+abs(y-center_y) <= number - 1
+
+def bomb(bomb_row, bomb_col):
+    #step1. 십자모양 폭탄 터치기
+    number = grid[bomb_row][bomb_col]
+    for i in range(n):
+        for j in range(n):
+            if bomb_in_range(i,j,bomb_row,bomb_col,number):
+                grid[i][j] = 0
+    
+    #step2. 떨어지는 모양 next_grid에 담기
+    for i in range(n):
+        for j in range(n):
+            next_grid[i][j] = 0
+    
     for i in range(n):
         temp_row = n-1
         for j in range(n-1,-1,-1):
-            if grid[j][i] == 0:
+            if not grid[j][i]:
                 continue
-            temp_grid[temp_row][i] = grid[j][i]
+            next_grid[temp_row][i] = grid[j][i]
             temp_row -= 1
     
+    #step3. next_grid -> grid로 옮기기
     for i in range(n):
         for j in range(n):
-            grid[i][j] = temp_grid[i][j]
+            grid[i][j] = next_grid[i][j]
 
-def bomb(x,y):
-    for i in range(n):
-        for j in range(n):
-            if x==i and y==j:
-                continue
-            if (x==i or y==j) and (abs(x-i)+abs(y-j) <= grid[x][y]-1):
-                grid[i][j] = 0
-    grid[x][y] = 0
-
-def choise_bomb(col):
-    for i in range(n):
-        if grid[i][col] > 0:
-            return i
-    return -1
-
-def simulate(col):
-    x = choise_bomb(col)
-    if x == -1:
-        return
-    bomb(x,col)
-    shift_down()
 
 for _ in range(m):
-    c = int(input())
-    simulate(c-1)
+    bomb_col = int(input()) - 1
 
-for row in grid:
-    for elem in row:
-        print(elem, end=' ')
+    bomb_row = get_bomb_row(bomb_col)
+
+    if bomb_row == OUT_OF_GRID:
+        continue
+    
+    bomb(bomb_row, bomb_col)
+
+for i in range(n):
+    for j in range(n):
+        print(grid[i][j], end=' ')
     print()
