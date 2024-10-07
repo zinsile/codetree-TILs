7a0ac0cffe5558f61e72b625a7a987289c258ea0
sil <97718539+zinsile@users.noreply.github.com>
2024-10-07 17:57:10 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241007/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
+++ 241007/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
@@ -25,14 +25,11 @@ escape = False
 while True:
     if escape:
         break
-    time += 1
     nx, ny = x + dx[d], y + dy[d]
-    if nx == start_x - 1 and ny == start_y - 1:
-        break
     # 1. 새로 가려는 곳이 격자 밖이라면
     if not in_range(nx, ny):
         escape = True
-        # time += 1
+        time += 1
         break
 
     # 2. 새로 가려는 곳이 벽이라면:
@@ -43,28 +40,33 @@ while True:
             nx, ny = x + dx[d], y + dy[d]
             if not in_range(nx, ny):
                 escape = True
-                # time += 1
+                time += 1
                 break
             if grid[nx][ny] == 0:
                 # 이동
-                # time+=1
+                time += 1
                 x, y = nx, ny
                 break
+    if escape:
+        break
     nd = (d + 1) % 4
     # 3. 새로 가려는 곳이 비어있고 새로 가려는곳 아래가 벽이라면:
     if grid[nx][ny] == 0 and grid[nx + dx[nd]][ny] == 1:
         # 3.1 새로 가려는 곳으로 전진
-        # time += 1
+        time += 1
         x, y = nx , ny
 
     # 4. 새로 가려는 곳이 비어있고 새로 가려는곳 아래가 길이라면:
     if grid[nx][ny] == 0 and grid[nx + dx[nd]][ny] == 0:
         # 4.1 한칸 앞으로(새로 가려는 곳) 시계방향 회전한 뒤 , 다시 한칸 앞으로
-        x, y = nx + dx[nd], ny
-        time += 1
-        d = (d + 1) % 4
+        x, y = nx+dx[nd], ny+dy[nd]
+        time += 2
+        d = nd
+
+    if x == start_x - 1 and y == start_y - 1:
+        break
 
 if escape:
-    print(time - 1)
+    print(time)
 else:
     print(-1)
