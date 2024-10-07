7f040d585fc31edab0e164dc5e831300f926afad
sil <97718539+zinsile@users.noreply.github.com>
2024-10-07 16:04:50 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 116ms, Memory: 25MB, Status: Passed - CodeTree

--- 241007/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
+++ 241007/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
@@ -6,17 +6,18 @@ def in_range(x,y):
 
 x, y = r-1, c-1
 dxs, dys = [-1,1,0,0], [0,0,-1,1]
-#step0. 현재 위치 프린트
-print(grid[x][y], end=' ')
+
 while True:
+    #step0. 현재 위치 프린트
+    print(grid[x][y], end=' ')
     #step1. 상하좌우 탐색 : 현재보다 더 큰 거 있을때 바로 탐색 종료
     did_move = False
     for dx,dy in zip(dxs, dys):
         if in_range(x+dx, y+dy) and grid[x][y] < grid[x+dx][y+dy]:
             #step2. 이동
             x, y = x+dx, y+dy
-            print(grid[x][y], end=' ')
             did_move = True
+            break
     #    1.1. 만약 없을 경우 종료
     if not did_move:
         break
