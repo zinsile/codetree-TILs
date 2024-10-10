3d80f8fde71a54a6d66f6c719c7064c9474d2808
sil <97718539+zinsile@users.noreply.github.com>
2024-10-10 18:40:17 +0900

    Title: 고대 문명 유적 탐사, Time: 279ms, Memory: 26MB, Status: Passed - CodeTree

--- 241010/고대 문명 유적 탐사/ancient-ruin-exploration.py
+++ 241010/고대 문명 유적 탐사/ancient-ruin-exploration.py
@@ -6,44 +6,46 @@ ans = []
 
 def in_range(x,y):
     return 0<=x<5 and 0<=y<5
+def bfs(i,j,arr,v,flag):
+    q = []
+    spot = set()
+    q.append((i, j))
+    spot.add((i, j))
+    v[i][j] = True
+    cnt = 1
+
+    while q:
+        x, y = q.pop(0)
+        for dx, dy in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+            nx, ny = x + dx, y + dy
+            # 범위내, 미방문, 조건: [x][y]와 같은 값
+            if in_range(nx, ny) and not v[nx][ny] and arr[x][y] == arr[nx][ny]:
+                q.append((nx, ny))
+                spot.add((nx, ny))
+                v[nx][ny] = True
+                cnt += 1
+    if cnt >= 3:
+        if flag == 1:
+            # 유물자리 초기화
+            for x, y in spot:
+                arr[x][y] = 0
+        return cnt, arr
+    return 0, arr
 
 def value_count(arr,flag):
     v = [[False]*5 for _ in range(5)]
     result = 0
     for i in range(5):
         for j in range(5):
-            q = []
-            spot = set()
-            q.append((i,j))
-            spot.add((i,j))
-            v[i][j] = True
-            cnt = 1
-
-            while q:
-                x,y = q.pop(0)
-                for dx,dy in [(-1,0),(0,1),(1,0),(0,-1)]:
-                    nx, ny = x+dx, y+dy
-                    # 범위내, 미방문, 조건: [x][y]와 같은 값
-                    if in_range(nx,ny) and not v[nx][ny] and arr[x][y] == arr[nx][ny]:
-                        q.append((nx,ny))
-                        spot.add((nx,ny))
-                        v[nx][ny] = True
-                        cnt += 1
-            if cnt >= 3:
-                if flag == 1:
-                    # 유물자리 초기화
-                    for x, y in spot:
-                        arr[x][y] = 0
-                result += cnt
+            cnt,arr = bfs(i,j,arr,v,flag)
+            result += cnt
     return result, arr
 
 def rotate(arr,x,y,rot):
-    for _ in range(rot):
-        narr = [x[:] for x in arr]
-        for i in range(3):
-            for j in range(3):
-                narr[x+i][y+j] = arr[x+3-j-1][y+i]
-        arr = narr
+    narr = [x[:] for x in arr]
+    for i in range(3):
+        for j in range(3):
+            narr[x+i][y+j] = arr[x+3-j-1][y+i]
     return narr
 
 
@@ -53,10 +55,11 @@ for _ in range(K):
     # 최종 회전 격자 구하기 : 각도->열->행
     max_cnt = 0
     for rot in range(1,4):
-        for i in range(3):
-            for j in range(3):
+        for j in range(3):
+            for i in range(3):
                 narr = [x[:] for x in arr]
-                narr = rotate(narr,i,j,rot) # rot 회수만큼 시계방향 90도 회전
+                for _ in range(rot):
+                    narr = rotate(narr,i,j,rot) # rot 회수만큼 시계방향 90도 회전
                 cnt, narr = value_count(narr,0)
                 if max_cnt < cnt:
                     max_cnt = cnt
@@ -66,17 +69,17 @@ for _ in range(K):
         break
     # [2] 유물획득
     arr = marr
-    cnt = 0
+    tc_ans = 0
     while True: # 유물 획득 안될때까지 획득
         t, arr = value_count(arr,1)
 
         if t == 0: #만약 탐사 안된다면 이번 턴 종료
             break
-        cnt += t
+        tc_ans += t
         # 유물자리 채우기
         for y in range(5):
             for x in range(4,-1,-1):
                 if arr[x][y] == 0:
                     arr[x][y] = wall.pop(0)
-    ans.append(cnt)
+    ans.append(tc_ans)
 print(*ans)
